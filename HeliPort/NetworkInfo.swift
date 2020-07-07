ddbb4eaa1e433f8ff7e2d6922c08048331ec3fba
Igor Kulman <igor@kulman.sk>
2020-07-07 13:40:24 +0200

    Cleaning up the method to get IP address

--- HeliPort/NetworkInfo.swift
+++ HeliPort/NetworkInfo.swift
@@ -250,33 +250,45 @@ class NetworkManager {
     class func getLocalAddress(bsd: String) -> String? {
         // Get list of all interfaces on the local machine:
         var ifaddr: UnsafeMutablePointer<ifaddrs>?
-        guard getifaddrs(&ifaddr) == 0 else { return nil }
-        guard let firstAddr = ifaddr else { return nil }
+        guard getifaddrs(&ifaddr) == 0, let firstAddr = ifaddr else {
+            return nil
+        }
+
         var ipV4: String?
         var ipV6: String?
+
         // For each interface ...
         for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
             let interface = ifptr.pointee
+
             // Check for IPv4 or IPv6 interface:
             let addrFamily = interface.ifa_addr.pointee.sa_family
-            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
-                // Check interface name:
-                let name = String(cString: interface.ifa_name)
-                if name == bsd {
-                    // Convert interface address to a human readable string:
-                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
-                                &hostname, socklen_t(hostname.count),
-                                nil, socklen_t(0), NI_NUMERICHOST)
-                    if addrFamily == UInt8(AF_INET) {
-                        ipV4 = String(cString: hostname)
-                    } else if addrFamily == UInt8(AF_INET6) {
-                        ipV6 = String(cString: hostname)
-                    }
-                }
+            guard addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) else {
+                continue
+            }
+
+            // Check interface name:
+            let name = String(cString: interface.ifa_name)
+            guard name == bsd else {
+                continue
+            }
+
+            // Convert interface address to a human readable string:
+            var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+            getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                        &hostname, socklen_t(hostname.count),
+                        nil, socklen_t(0), NI_NUMERICHOST)
+
+            if addrFamily == UInt8(AF_INET) {
+                ipV4 = String(cString: hostname)
+            } else if addrFamily == UInt8(AF_INET6) {
+                ipV6 = String(cString: hostname)
             }
         }
+
         freeifaddrs(ifaddr)
+
+        // ipV4 has priority
         return ipV4 ?? ipV6
     }
 }
