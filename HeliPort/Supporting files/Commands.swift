3af6b5f1b59e9d695f167ce3ffba5cadaf37f620
Bat.bat <williambj1@users.noreply.github.com>
2021-01-14 20:20:30 +0800

    Update Podfile.lock, Add missing License

--- HeliPort/Supporting files/Commands.swift
+++ HeliPort/Supporting files/Commands.swift
@@ -7,11 +7,11 @@
 //
 
 /*
-* This program and the accompanying materials are licensed and made available
-* under the terms and conditions of the The 3-Clause BSD License
-* which accompanies this distribution. The full text of the license may be found at
-* https://opensource.org/licenses/BSD-3-Clause
-*/
+ * This program and the accompanying materials are licensed and made available
+ * under the terms and conditions of the The 3-Clause BSD License
+ * which accompanies this distribution. The full text of the license may be found at
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
 
 import Foundation
 
