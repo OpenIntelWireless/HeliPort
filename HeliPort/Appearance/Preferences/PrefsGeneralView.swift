OpenIntelWireless/HeliPort

commit 8972f7fdd14e1746bc73939aa11a0f5d161fa6dc

Silence warnings

HeliPort/Appearance/Preferences/PrefsGeneralView.swift
@@ -27,7 +27,7 @@ class PrefsGeneralView: NSView {
 
     let autoUpdateCheckbox: NSButton = {
         let checkbox = NSButton(checkboxWithTitle: .autoCheckUpdate,
-                                target: self,
+                                target: NSButton.self,
                                 action: #selector(checkboxChanged(_:)))
         checkbox.identifier = .autoUpdateId
         return checkbox
@@ -35,7 +35,7 @@ class PrefsGeneralView: NSView {
 
     let autoDownloadCheckbox: NSButton = {
         let checkbox = NSButton(checkboxWithTitle: .autoDownload,
-                                target: self,
+                                target: NSButton.self,
                                 action: #selector(checkboxChanged(_:)))
         checkbox.identifier = .autoDownloadId
         return checkbox

HeliPort/AppDelegate.swift
@@ -40,7 +40,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let interface = String(cString: &drv_info.bsd_name.0)
         guard !version.isEmpty, !interface.isEmpty else {
             Log.error("itlwm kext not loaded!")
-            #if !DEBUG
+#if !DEBUG
             let alert = CriticalAlert(message: NSLocalizedString("itlwm is not running"),
                                       options: [NSLocalizedString("Dismiss"),
                                                 NSLocalizedString("Quit HeliPort")])
@@ -49,7 +49,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 NSApp.terminate(nil)
             }
 
-            #endif
+#endif
             return false
         }
 
@@ -64,16 +64,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private func checkRunPath() {
         let pathComponents = (Bundle.main.bundlePath as NSString).pathComponents
 
-        #if DEBUG
+#if DEBUG
         // Normal users should never use the Debug Version
         guard pathComponents[pathComponents.count - 2] != "Debug" else {
             return
         }
-        #else
+#else
         guard pathComponents[pathComponents.count - 2] != "Applications" else {
             return
         }
-        #endif
+#endif
 
         Log.error("Running path unexpected!")
 
@@ -94,9 +94,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         Log.error("itlwm API mismatch!")
 
-        #if !DEBUG
+#if !DEBUG
         let text = NSLocalizedString("HeliPort API Version: ") + String(IOCTL_VERSION) +
-            "\n" + NSLocalizedString("itlwm API Version: ") + String(drv_info.version)
+                   "\n" + NSLocalizedString("itlwm API Version: ") + String(drv_info.version)
         let alert = CriticalAlert(message: NSLocalizedString("itlwm Version Mismatch"),
                                   informativeText: text,
                                   options: [NSLocalizedString("Quit HeliPort"),
@@ -109,7 +109,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
 
         NSApp.terminate(nil)
-        #endif
+#endif
     }
 
     func applicationWillTerminate(_ notification: Notification) {

HeliPort/Appearance/Preferences/PrefsSavedNetworksView.swift
@@ -91,7 +91,7 @@ class PrefsSavedNetworksView: NSView {
         }
         let button = NSSegmentedControl(images: [removeImage, viewImage],
                                         trackingMode: .momentary,
-                                        target: self,
+                                        target: NSSegmentedControl.self,
                                         action: #selector(modifyItemClicked(_:)))
         button.setEnabled(false, forSegment: .remove)
         button.setEnabled(false, forSegment: .view)

commit 4116550237076c30543ed104512b636af69a4760

Fix more regression introduced in 8972f7f

HeliPort/Appearance/Preferences/PrefsGeneralView.swift
@@ -22,20 +22,21 @@ class PrefsGeneralView: NSView {
 
     let updatesLabel: NSTextField = {
         let view = NSTextField(labelWithString: .startup)
+        view.alignment = .right
         return view
     }()
 
-    let autoUpdateCheckbox: NSButton = {
+    lazy var autoUpdateCheckbox: NSButton = {
         let checkbox = NSButton(checkboxWithTitle: .autoCheckUpdate,
-                                target: NSButton.self,
+                                target: self,
                                 action: #selector(checkboxChanged(_:)))
         checkbox.identifier = .autoUpdateId
         return checkbox
     }()
 
-    let autoDownloadCheckbox: NSButton = {
+    lazy var autoDownloadCheckbox: NSButton = {
         let checkbox = NSButton(checkboxWithTitle: .autoDownload,
-                                target: NSButton.self,
+                                target: self,
                                 action: #selector(checkboxChanged(_:)))
         checkbox.identifier = .autoDownloadId
         return checkbox